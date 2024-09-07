1d6abc9c2fdd36791cb7f354b4f95a7b535753a6
dudeperf3ct <dudeperf3ct@users.noreply.github.com>
2024-09-07 19:13:50 +0530

    :art: Update reamde

--- README.md
+++ README.md
@@ -1,10 +1,16 @@
 # Web Vibe
 
+Idea: Create a project that helps content writers and graphic designers evaluate their website with help of LLMs.
+
+Check [developer logs](./Developer%20Logs.md) for details on the progress.
+
 ## Getting Started
 
 Pre-requisities
 
 - [uv](https://docs.astral.sh/uv/)
+
+## Locally
 
 1. Create a virtual environment and install dependencies using `uv`.
 
@@ -16,6 +22,10 @@ Pre-requisities
     ```
 
 2. Add API secrets to `.env` file.
+
+    ```text
+    ANTHROPIC_API_KEY='your-key-here'
+    ```
 
 3. Run the python script.
 
--- visualize_result.py
+++ visualize_result.py
@@ -8,11 +8,11 @@ DATA_DIR = "data"
 
 
 def main():
+    """Streamlit entrypoint."""
 
     st.title("Web Vibe")
-
     data_path = f"{DATA_DIR}/analysis.json"
-
+    # Check if results file is present
     if not os.path.exists(data_path):
         raise ValueError("Run `main.py` to create a analysis json file.")
 
@@ -21,9 +21,7 @@ def main():
 
     urls = [d["url"] for d in data]
     selected_url = st.selectbox(label="Select website to analyse", options=urls)
-
     selected_index = urls.index(selected_url)
-
     col1, col2 = st.columns(2)
     with col1:
         st.image(data[selected_index]["screenshot"])
